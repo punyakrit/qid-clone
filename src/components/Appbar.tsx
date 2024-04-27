05c90032666681a0631dfad555db482dffc972da
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-28 01:01:47 +0530

    contact

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import { RxCross2 } from "react-icons/rx";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { IoMenu } from "react-icons/io5";
+import { RxCross2 } from "react-icons/rx";
 
 function Appbar() {
   const [menu, setMenu] = useState(false);
+  const location = useLocation();
+  const Navigation = useNavigate();
 
   function toggle() {
     setMenu(!menu);
   }
-  const Navigation = useNavigate();
+
   return (
     <div>
       <div className="fixed 2xl:px-[750px] xl:px-44 px-12 border-b-[1px] border-gray-600/20 items-center text-white z-20 w-full h-24 bg-black flex justify-between">
@@ -17,7 +19,9 @@ function Appbar() {
           onClick={() => {
             Navigation("/");
           }}
-          className="text-[38px] text-[#dfdfdf] font-medium italic cursor-pointer"
+          className={`text-[38px] font-medium italic cursor-pointer ${
+            location.pathname === "/" ? "text-white" : "text-[#dfdfdf]"
+          }`}
         >
           qid
         </div>
@@ -34,12 +38,14 @@ function Appbar() {
             />
           )}
         </div>
-        <div className="hidden md:flex space-x-12  text-[16px]">
+        <div className="hidden md:flex space-x-12 text-[16px]">
           <div
             onClick={() => {
               Navigation("/app");
             }}
-            className="text-[#7A7A7A] hover:text-white cursor-pointer transform duration-500"
+            className={`hover:text-white cursor-pointer transform duration-500 ${
+              location.pathname === "/app" ? "text-white" : "text-[#7A7A7A]"
+            }`}
           >
             App
           </div>
@@ -47,7 +53,11 @@ function Appbar() {
             onClick={() => {
               Navigation("/home/c-form-automation-for-hotels");
             }}
-            className="text-[#7A7A7A] hover:text-white cursor-pointer transform duration-500"
+            className={`hover:text-white cursor-pointer transform duration-500 ${
+              location.pathname === "/home/c-form-automation-for-hotels"
+                ? "text-white"
+                : "text-[#7A7A7A]"
+            }`}
           >
             C-Form pro
           </div>
@@ -55,7 +65,9 @@ function Appbar() {
             onClick={() => {
               Navigation("/support");
             }}
-            className="text-[#7A7A7A] hover:text-white cursor-pointer transform duration-500"
+            className={`hover:text-white cursor-pointer transform duration-500 ${
+              location.pathname === "/support" ? "text-white" : "text-[#7A7A7A]"
+            }`}
           >
             Contact
           </div>
@@ -68,7 +80,6 @@ function Appbar() {
               onClick={() => {
                 Navigation("/app");
                 setMenu(!menu);
-
               }}
               className="px-8 py-3 border-b border-black"
             >
@@ -78,7 +89,6 @@ function Appbar() {
               onClick={() => {
                 Navigation("/home/c-form-automation-for-hotels");
                 setMenu(!menu);
-
               }}
               className="px-8 py-3 border-b border-black"
             >
@@ -88,7 +98,6 @@ function Appbar() {
               onClick={() => {
                 Navigation("/support");
                 setMenu(!menu);
-
               }}
               className="px-8 py-3 border-b border-black"
             >
--- src/page/Contact.tsx
+++ src/page/Contact.tsx
@@ -1,10 +1,65 @@
-
+import { Mail, Phone } from "lucide-react";
+import bg from "../assets/bg2.png";
 function Contact() {
   return (
-    <div>
-      
+    <div
+      className="md:h-screen h-full border-b border-gray-500/30"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="py-32 md:pt-32 bg-black/80  pt h-full w-full text-white 2xl:px-[750px] xl:px-52 px-6">
+        <div className="md:flex h-full">
+          <div className="md:w-1/2 flex flex-col justify-center ">
+            <div className=" text-5xl lg:text-8xl font-medium">Contact Us</div>
+            <div className="text-2xl py-4 text-white/60">
+              Discover Qid – Your Partner in Secure Identity Management
+            </div>
+            <div className="space-y-5 pb-10">
+              <div className="flex text-lg font-medium items-center">
+                <Mail className="mr-4" />{" "}
+                <a
+                  href="mailto:contact@oneqid.com"
+                  className="text-red-400 hover:text-purple-900"
+                >
+                  contact@oneqid.com
+                </a>
+              </div>
+              <div className="flex text-lg font-medium items-center">
+                <Phone className="mr-4" />{" "}
+                <a href="tel:+919606406404">+91 960 640 640 4</a>
+              </div>
+            </div>
+            <div className="text-[#6fa7fd] border-t border-gray-500 lg:mr-40 md:mr-20 mr-10 py-10 text-3xl font-medium">
+              Trusted by 25,000+ Users
+            </div>
+          </div>
+          <div className="md:w-1/2 flex items-center lg:pl-6 justify-center">
+            <div className="border border-white/30 md:p-12 p-6 w-full h-min rounded-3xl space-y-5">
+              <Form label={"Name"} />
+              <div className="md:flex md:space-x-5 space-y-5 md:space-y-0">
+                <Form label={"Phone Number"} />
+                <Form label={"Email"} />
+              </div>
+              <Form label={"Message"} />
+              <div className="flex justify-end">
+                <div className="bg-white text-black md:w-min w-full text-center mt-6 font-normal text-lg py-4 px-10 cursor-pointer rounded-full transition duration-300 hover:bg-blue-500">
+                  Submit
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+function Form({ label }: any) {
+  return (
+    <div>
+      <div className="text-white/50 font-light">{label}</div>
+      <input className="bg-transparent border-b border-white/50 text-xl px-4 w-full p-2"></input>
+    </div>
+  );
+}
+
+export default Contact;
